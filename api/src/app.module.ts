4e031b2a639e2e056a0888991d5a2d011fff1f30
MarkTeryokhin <maaqicyk@gmail.com>
2026-01-20 20:50:00 +0300

    feat: integrate new modules into application

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,24 +5,32 @@ import { AppService } from './app.service';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { PositionsModule } from './positions/positions.module';
 import { DepartmentsModule } from './departments/departments.module';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { EmployeeFileModule } from './modules/employee-file/employee-file.module';
+import { PersonnelOperationModule } from './modules/personnel-operation/personnel-operation.module';
+import { ChangeHistoryModule } from './modules/change-history/change-history.module';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'areal_hr',
-            autoLoadEntities: true,
-            synchronize: false,
-        }),
-        OrganizationsModule,
-        PositionsModule,
-        DepartmentsModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'areal_hr',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    OrganizationsModule,
+    PositionsModule,
+    DepartmentsModule,
+    EmployeeModule,
+    EmployeeFileModule,
+    PersonnelOperationModule,
+    ChangeHistoryModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  
+  await app.listen(3000);
+  console.log(`Application is running on: http://localhost:3000/api`);
 }
 bootstrap();
